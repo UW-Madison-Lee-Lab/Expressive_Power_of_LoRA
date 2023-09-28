5cb9a9cf96ddc917adad11d15d853b8374cde990
Yuchen Zeng <yzeng58@wisc.edu>
2023-09-28 15:15:54 +0000

    update configs

--- configs/config.py
+++ configs/config.py
@@ -7,6 +7,7 @@ n_test = 5000
 std = .25
 batch_size = 256
 n_epochs = 5000
+width = 16
 
 fnn_configs = []
 
@@ -18,8 +19,62 @@ for init_mode in ['default', 'uniform_singular_values']:
     use_bias = 0
     activation = 'linear'
     
-    for width in [4, 8, 16]:
-        for frozen_depth in [2, 4, 8]:
+
+    for frozen_depth in [2, 4, 8]:
+        for rank in range(1, width + 1):
+            # sgd
+            method = 'sgd'
+            for lr in [1e-2, 1e-3, 1e-4]:
+                for weight_decay in [0, 1e-4, 1e-3, 1e-2]:
+                    config = (
+                        width, 
+                        target_depth, 
+                        frozen_depth, 
+                        rank, 
+                        use_bias,
+                        activation, 
+                        std, 
+                        method, 
+                        batch_size, 
+                        n_epochs,
+                        lr,
+                        n_test,
+                        weight_decay,
+                        init_mode, 
+                        exp,
+                        wandb,
+                    )
+                    fnn_configs.append(config)
+            # ours
+            method = 'ours'
+            config = (
+                width, 
+                target_depth, 
+                frozen_depth, 
+                rank, 
+                use_bias,
+                activation, 
+                std, 
+                method, 
+                batch_size, 
+                n_epochs,
+                lr,
+                n_test,
+                weight_decay,
+                init_mode, 
+                exp,
+                wandb,
+            )
+            fnn_configs.append(config)
+                
+    #fnn approximation
+    use_bias = 1
+    activation = 'relu'
+    
+    for target_depth in [1, 2, 4]:
+        frozen_depth_list = np.array([2, 4, 8]) * target_depth
+        for frozen_depth in frozen_depth_list:
+            tdl = frozen_depth // target_depth
             for rank in range(1, width + 1):
                 # sgd
                 method = 'sgd'
@@ -44,6 +99,7 @@ for init_mode in ['default', 'uniform_singular_values']:
                             wandb,
                         )
                         fnn_configs.append(config)
+            
                 # ours
                 method = 'ours'
                 config = (
@@ -65,62 +121,6 @@ for init_mode in ['default', 'uniform_singular_values']:
                     wandb,
                 )
                 fnn_configs.append(config)
-                
-    #fnn approximation
-    use_bias = 1
-    activation = 'relu'
-    
-    for target_depth in [1, 2, 4]:
-        for width in [4, 8, 16]:
-            frozen_depth_list = np.array([2, 4, 8]) * target_depth
-            for frozen_depth in frozen_depth_list:
-                tdl = frozen_depth // target_depth
-                for rank in range(1, width + 1):
-                    # sgd
-                    method = 'sgd'
-                    for lr in [1e-2, 1e-3, 1e-4]:
-                        for weight_decay in [0, 1e-4, 1e-3, 1e-2]:
-                            config = (
-                                width, 
-                                target_depth, 
-                                frozen_depth, 
-                                rank, 
-                                use_bias,
-                                activation, 
-                                std, 
-                                method, 
-                                batch_size, 
-                                n_epochs,
-                                lr,
-                                n_test,
-                                weight_decay,
-                                init_mode, 
-                                exp,
-                                wandb,
-                            )
-                            fnn_configs.append(config)
-                
-                    # ours
-                    method = 'ours'
-                    config = (
-                        width, 
-                        target_depth, 
-                        frozen_depth, 
-                        rank, 
-                        use_bias,
-                        activation, 
-                        std, 
-                        method, 
-                        batch_size, 
-                        n_epochs,
-                        lr,
-                        n_test,
-                        weight_decay,
-                        init_mode, 
-                        exp,
-                        wandb,
-                    )
-                    fnn_configs.append(config)
 
 # tfn
 exp = 'tfn'
@@ -132,50 +132,50 @@ std = .25
 batch_size = 256
 n_epochs = 5000
 tfn_configs = []
+embed_dim = 16
 
-for embed_dim in [4, 8, 16]:
-    for depth in [1, 2, 4]:
-        for rank in range(1, width + 1):
-            # sgd
-            method = 'sgd'
-            for lr in [1e-2, 1e-3, 1e-4]:
-                for weight_decay in [0, 1e-4, 1e-3, 1e-2]:
-                    config = (
-                        embed_dim,
-                        n_head, 
-                        depth,
-                        rank,
-                        batch_size,
-                        seq_len,
-                        method,
-                        n_epochs,
-                        lr,
-                        weight_decay,
-                        wandb,
-                        std,
-                        n_test,
-                        exp,
-                    )
-                    tfn_configs.append(config)
-            # ours
-            method = 'ours'
-            config = (
-                embed_dim,
-                n_head, 
-                depth,
-                rank,
-                batch_size,
-                seq_len,
-                method,
-                n_epochs,
-                lr,
-                weight_decay,
-                wandb,
-                std,
-                n_test,
-                exp,
-            )
-            tfn_configs.append(config)
+for depth in [1, 2, 4]:
+    for rank in range(1, width + 1):
+        # sgd
+        method = 'sgd'
+        for lr in [1e-2, 1e-3, 1e-4]:
+            for weight_decay in [0, 1e-4, 1e-3, 1e-2]:
+                config = (
+                    embed_dim,
+                    n_head, 
+                    depth,
+                    rank,
+                    batch_size,
+                    seq_len,
+                    method,
+                    n_epochs,
+                    lr,
+                    weight_decay,
+                    wandb,
+                    std,
+                    n_test,
+                    exp,
+                )
+                tfn_configs.append(config)
+        # ours
+        method = 'ours'
+        config = (
+            embed_dim,
+            n_head, 
+            depth,
+            rank,
+            batch_size,
+            seq_len,
+            method,
+            n_epochs,
+            lr,
+            weight_decay,
+            wandb,
+            std,
+            n_test,
+            exp,
+        )
+        tfn_configs.append(config)
 
 new_fnn_configs = pd.DataFrame(fnn_configs)
 new_tfn_configs = pd.DataFrame(tfn_configs)
@@ -184,7 +184,7 @@ if os.path.exists('fnn_configs.csv'):
     origin_fnn_configs = pd.read_csv('fnn_configs.csv', header=None)
     
     # Find additional configs
-    additional_fnn_configs = pd.concat([origin_fnn_configs, new_fnn_configs]).drop_duplicates(keep=False)
+    additional_fnn_configs = new_fnn_configs.merge(origin_fnn_configs, how='left', indicator=True).loc[lambda x : x['_merge']=='left_only'].drop('_merge', axis=1).reset_index(drop=True)
     additional_fnn_configs.to_csv('additional_fnn_configs.csv', index=False, header=False)
     
     while 1:
@@ -206,7 +206,7 @@ if os.path.exists('tfn_configs.csv'):
     origin_tfn_configs = pd.read_csv('tfn_configs.csv', header=None)
     
     # Find additional configs
-    additional_tfn_configs = pd.concat([origin_tfn_configs, new_tfn_configs]).drop_duplicates(keep=False)
+    additional_tfn_configs = new_tfn_configs.merge(origin_tfn_configs, how='left', indicator=True).loc[lambda x : x['_merge']=='left_only'].drop('_merge', axis=1).reset_index(drop=True)
     additional_tfn_configs.to_csv('additional_tfn_configs.csv', index=False, header=False)
     
     while 1:
