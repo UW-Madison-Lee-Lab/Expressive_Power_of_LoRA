6bdc3d8c32f74f57bdcc689ea0e16301831c0c25
Yuchen Zeng <yzeng58@wisc.edu>
2023-11-11 23:40:39 +0000

    update

--- references/examples/NLU/roberta_base_cola.sh
+++ references/examples/NLU/roberta_base_cola.sh
@@ -1,4 +1,5 @@
-export num_gpus=2
+# export num_gpus=2
+export num_gpus=1
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 path=$(pwd)
