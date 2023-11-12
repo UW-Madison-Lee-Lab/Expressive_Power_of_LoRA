bc0d13eaa008eab576d3a359fa8d4e0c6ff28fbd
Yuchen Zeng <yzeng58@wisc.edu>
2023-11-12 20:08:28 +0000

    fix the bug

--- references/examples/NLU/roberta_base_cola.sh
+++ references/examples/NLU/roberta_base_cola.sh
@@ -1,5 +1,5 @@
 # export num_gpus=2
-export num_gpus=1
+export num_gpus=2
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 export PYTHONPATH="$(pwd)/src:$PYTHONPATH"  
@@ -9,7 +9,7 @@ output_dir=$root_dir/results/real_exp/NLU/cola
 
 python -m torch.distributed.launch --nproc_per_node=$num_gpus \
 examples/text-classification/run_glue.py \
---model_name_or_path roberta-large \
+--model_name_or_path roberta-base \
 --task_name cola \
 --do_train \
 --do_eval \
@@ -17,7 +17,7 @@ examples/text-classification/run_glue.py \
 --per_device_train_batch_size 16 \
 --learning_rate 4e-4 \
 --num_train_epochs 1 \
---output_dir $output_dir/model \23
+--output_dir $output_dir/model \
 --overwrite_output_dir \
 --logging_steps 10 \
 --logging_dir $output_dir/log \
