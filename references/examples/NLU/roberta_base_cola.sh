496b44a1e2a1a14cf28267c7da73396ddfc1976e
Yuchen Zeng <yzeng58@wisc.edu>
2023-11-12 02:09:19 +0000

    try mnlii

--- references/examples/NLU/roberta_base_cola.sh
+++ references/examples/NLU/roberta_base_cola.sh
@@ -9,7 +9,7 @@ output_dir=$root_dir/results/real_exp/NLU/cola
 
 python -m torch.distributed.launch --nproc_per_node=$num_gpus \
 examples/text-classification/run_glue.py \
---model_name_or_path roberta-base \
+--model_name_or_path roberta-large \
 --task_name cola \
 --do_train \
 --do_eval \
@@ -30,6 +30,30 @@ examples/text-classification/run_glue.py \
 --seed 0 \
 --weight_decay 0.1 \
 --pretrained 0
+
+# python -m torch.distributed.launch --nproc_per_node=$num_gpus \
+# examples/text-classification/run_glue.py \
+# --model_name_or_path roberta-base \
+# --task_name cola \
+# --do_train \
+# --do_eval \
+# --max_seq_length 512 \
+# --per_device_train_batch_size 16 \
+# --learning_rate 4e-4 \
+# --num_train_epochs 1 \
+# --output_dir $output_dir/model \
+# --overwrite_output_dir \
+# --logging_steps 10 \
+# --logging_dir $output_dir/log \
+# --evaluation_strategy epoch \
+# --save_strategy epoch \
+# --warmup_ratio 0.06 \
+# --apply_lora \
+# --lora_r 8 \
+# --lora_alpha 16 \
+# --seed 0 \
+# --weight_decay 0.1 \
+# --pretrained 0
 
 # --per_device_train_batch_size 32
 # --pretrained 1
